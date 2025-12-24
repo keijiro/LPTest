31956e7f8f8915b50e07f230564bd121189b33d1
Keijiro Takahashi <keijiro@gmail.com>
2025-12-24 13:53:44 +0900

    Refereence setup

---
+++ References/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+EDITOR_BASE="/Applications/Unity/Hub/Editor"
+
+if [ ! -d Documentation ]; then
+  mkdir Documentation
+  LATEST_EDITOR=`ls $EDITOR_BASE | sort -V | tail -n 1`
+  cp -r $EDITOR_BASE/$LATEST_EDITOR/Documentation/en/ScriptReference/LowLevelPhysics* Documentation
+fi
+
+if [ ! -d LowLevelSamples ]; then
+    git clone git@github.com:Unity-Technologies/PhysicsExamples2D.git
+    mv PhysicsExamples2D/LowLevel LowLevelSamples
+    rm -rf PhysicsExamples2D
+fi
